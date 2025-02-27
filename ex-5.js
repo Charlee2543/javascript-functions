02207c844a045c7af497a8632dab70f35ef1b1b4
Pongsakon <charlee2543@hotmail.com>
2025-02-27 13:50:16 +0700

    feat: charlee add Ex2-6

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,20 @@
 // Start coding here
+// function findMinNumber(a, b) {
+// 	let lastNumber = 0;
+// 	if (a > b) {
+// 		lastNumber = b;
+// 	} else if (a < b) {
+// 		lastNumber = a;
+// 	} else {
+// 		lastNumber = "no number or The numbers are the same";
+// 	}
+// 	return lastNumber;
+// }
+function findMinNumber(a, b) {
+	let lastNumber = Math.min(a, b);
+	return lastNumber;
+}
+
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
